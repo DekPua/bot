d5253a8bbf963bab3c416f2b85317ce777f3ac5d
HewkawAr <hewkawar@gmail.com>
2024-04-19 23:35:30 +0700

    Update ban.js

--- functions/ban.js
+++ functions/ban.js
@@ -2,17 +2,20 @@ const { EmbedBuilder } = require("discord.js");
 
 module.exports = (client) => {
     client.ban = async (member, reasons) => {
-        await member.send({
-            embeds: [
-                new EmbedBuilder()
-                    .setColor('Red')
-                    .setDescription(`⛔ คุณถูกแบน\n\`\`\`${reasons.th ?? null}\`\`\`\nหากคิดว่าเป็นเรื่องเข้าใจผิดกรุณาส่งเรื่องไปยัง **[DekPua Support](https://discord.com/channels/1213126282921902230/1224718613131563028)**.\n\n⛔ You have been banned\n\`\`\`${reasons.en ?? null}\`\`\`\n If there must be a misunderstanding, please send the matter to **[DekPua Support](https://discord.com/channels/1213126282921902230/1224718613131563028)**.`)
-                    .setTimestamp(Date.now())
-            ]
-        });
+        try {
+            await member.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor('Red')
+                        .setDescription(`⛔ คุณถูกแบน\n\`\`\`${reasons.th ?? null}\`\`\`\nหากคิดว่าเป็นเรื่องเข้าใจผิดกรุณาส่งเรื่องไปยัง **[DekPua Support](https://discord.com/channels/1213126282921902230/1224718613131563028)**.\n\n⛔ You have been banned\n\`\`\`${reasons.en ?? null}\`\`\`\n If there must be a misunderstanding, please send the matter to **[DekPua Support](https://discord.com/channels/1213126282921902230/1224718613131563028)**.`)
+                        .setTimestamp(Date.now())
+                ]
+            });
+
+        } catch (err) { }
 
         const BannedRole = await member.guild.roles.cache.get('1224776064769720371');
-
+    
         const hasBanned = await member.roles.cache.has(BannedRole);
 
         if (!hasBanned) await member.roles.add(BannedRole);
@@ -21,14 +24,17 @@ module.exports = (client) => {
     }
 
     client.timeout = async (member, time, reasons) => {
-        await member.send({
-            embeds: [
-                new EmbedBuilder()
-                    .setColor('Red')
-                    .setDescription(`⛔ บัญชีถูกปิดใช้งานชั่วคราว\n\`\`\`${reasons.th ?? null}\`\`\`\nหากคิดว่าเป็นเรื่องเข้าใจผิดกรุณาส่งเรื่องไปยัง **[DekPua Support](https://discord.com/channels/1213126282921902230/1224718613131563028)**.\n\n⛔ You have been timeout\n\`\`\`${reasons.en ?? null}\`\`\`\n If there must be a misunderstanding, please send the matter to **[DekPua Support](https://discord.com/channels/1213126282921902230/1224718613131563028)**.`)
-                    .setTimestamp(Date.now())
-            ]
-        });
+        try {
+            await member.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor('Red')
+                        .setDescription(`⛔ บัญชีถูกปิดใช้งานชั่วคราว\n\`\`\`${reasons.th ?? null}\`\`\`\nหากคิดว่าเป็นเรื่องเข้าใจผิดกรุณาส่งเรื่องไปยัง **[DekPua Support](https://discord.com/channels/1213126282921902230/1224718613131563028)**.\n\n⛔ You have been timeout\n\`\`\`${reasons.en ?? null}\`\`\`\n If there must be a misunderstanding, please send the matter to **[DekPua Support](https://discord.com/channels/1213126282921902230/1224718613131563028)**.`)
+                        .setTimestamp(Date.now())
+                ]
+            });
+
+        } catch (err) { }
 
         await member.timeout(time, `\`\`\`TH: ${reasons.th}\nEN: ${reasons.en}\`\`\``);
 
